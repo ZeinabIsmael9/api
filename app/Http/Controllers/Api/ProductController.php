da87a6cde28bd1e1ab9b4f237ed9b32e204adafb
Zeinab Ismael <153861416+ZeinabIsmael9@users.noreply.github.com>
2025-06-19 17:57:49 +0300

    make the filter method

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -14,39 +14,19 @@ class ProductController extends Controller
     {
         $cacheKey = 'products_' . md5(json_encode($request->all()));
         $products = Cache::remember($cacheKey, 600, function () use ($request) {
-            $query = Product::query();
-            if ($request->has('sort_by')) {
-                $sort = explode(',', $request->sort_by);
-                $column = $sort[0] ?? 'created_at';
-                $direction = $sort[1] ?? 'asc';
-                if (in_array($column, ['price', 'created_at']) && in_array($direction, ['asc', 'desc'])) {
-                    $query->orderBy($column, $direction);
-                }
-            }
-
-            if ($request->has('category_id')) {
-                $query->where('category_id', $request->category_id);
-            }
-
-            if ($request->has('min_price')) {
-                $query->where('price', '>=', $request->min_price);
-            }
-
-            if ($request->has('max_price')) {
-                $query->where('price', '<=', $request->max_price);
-            }
-
-            if ($request->has('in_stock')) {
-                $query->where('in_stock', $request->in_stock);
-            }
-
-            return $query->get();
+            return $this->filter($request);
         });
-
         return response()->json($products);
     }
 
     public function export(Request $request)
+    {
+        $products = $this->filter($request);
+        $pdf = Pdf::loadView('pdf.products', compact('products'));
+        return $pdf->download('filtered_products.pdf');
+    }
+
+    public function filter($request)
     {
         $query = Product::query();
         if ($request->has('sort_by')) {
@@ -74,8 +54,6 @@ class ProductController extends Controller
             $query->where('in_stock', $request->in_stock);
         }
 
-        $products = $query->get();
-        $pdf = Pdf::loadView('pdf.products', compact('products'));
-        return $pdf->download('filtered_products.pdf');
+        return $query->get();
     }
 }
